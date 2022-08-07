78988bd0360630233bd56dc3b31cd755fde4a9e6
Oleh Hasii <olehhasii@gmail.com>
2022-08-07 15:27:27 +0300

    updated styles for main page

--- src/main/components/CardsList.jsx
+++ src/main/components/CardsList.jsx
@@ -7,12 +7,7 @@ import fullBody from '../../assets/fullbody.png';
 const CardsList = () => {
 	return (
 		<div className='mt-12 flex flex-wrap justify-around'>
-			<MainCard
-				title='Try new workout'
-				className='h-60 bg-magnolia mb-10'
-				flex='flex'
-				width='w-2/3'
-			>
+			<MainCard className=' h-72 bg-magnolia mb-10' flex='flex' width='w-2/3'>
 				<img src={fullBody} alt='fullBody' className='h-full p-2' />
 				<div className='flex flex-col justify-around'>
 					<h2 className='text-2xl font-extrabold'>Full body</h2>
@@ -27,25 +22,52 @@ const CardsList = () => {
 					</Button>
 				</div>
 			</MainCard>
-			<MainCard title='Time this week' className='bg-lightGreen w-56 h-44'>
-				<span className='text-lg font-bold'>27 Hours ⌛</span>
-			</MainCard>
-			<MainCard
-				title='Workouts done this week💪'
-				className='w-64 h-28 bg-uraniumBlue'
-			>
-				<span className='text-lg font-bold'>0</span>
-				<span className='text-lg font-bold'>This week</span>
-			</MainCard>
-			<MainCard title='Todays workout' className='bg-spanish h-28 w-96'>
+			<MainCard title='Todays workout' className='bg-spanish h-72 w-96'>
+				<h2 className='text-xl font-bold mb-3'>Todays workout</h2>
 				<span className='text-lg font-bold mb-3'>Chest + triceps</span>
 				<Button variant='contained' color='success'>
 					Start workout
 				</Button>
 			</MainCard>
-			<MainCard title='Water balance' className='bg-uraniumBlue h-28 w-72'>
-				<span className='text-lg font-bold mb-3'>6/8 glasses 💧</span>
-				<Button variant='contained'>Add glass</Button>
+			<MainCard
+				width='w-1/4'
+				className='bg-lightGreen h-44'
+				flex='flex items-center justify-center'
+			>
+				<div className='mr-6 text-4xl bg-white w-14 h-16 flex items-center justify-center rounded-2xl'>
+					⌛
+				</div>
+				<div>
+					<span className='text-lg font-bold'>27h</span>
+					<h2 className='text-gray-600'>Time this week</h2>
+				</div>
+			</MainCard>
+			<MainCard
+				width='w-1/4'
+				className='h-44 bg-uraniumBlue '
+				flex='flex items-center justify-center'
+			>
+				<div className='mr-6 text-4xl bg-white w-14 h-16 flex items-center justify-center rounded-2xl'>
+					💪
+				</div>
+				<div>
+					<span className='text-lg font-bold'>0</span>
+					<h2 className='text-gray-600'>Workouts this week</h2>
+				</div>
+			</MainCard>
+
+			<MainCard
+				width='w-1/4'
+				className='bg-uraniumBlue h-44 '
+				flex='flex items-center justify-center'
+			>
+				<div className='mr-6 text-4xl bg-white w-14 h-16 flex items-center justify-center rounded-2xl'>
+					💧
+				</div>
+				<div>
+					<span className='text-lg font-bold block mb-2'>6/8 glasses</span>
+					<Button variant='contained'>Add glass</Button>
+				</div>
 			</MainCard>
 		</div>
 	);
--- src/main/components/MainCard.jsx
+++ src/main/components/MainCard.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 const MainCard = (props) => {
 	return (
 		<div className={`${props.width}`}>
-			<h2 className='text-xl font-bold underline text-center'>{props.title}</h2>
+			{/* <h2 className='text-xl font-bold underline text-center'>{props.title}</h2> */}
 			<div
 				className={`${
 					props.flex || 'flex flex-col justify-center items-center'
-				} rounded-xl ${props.className} `}
+				} rounded-xl ${props.className} shadow-top`}
 			>
 				{props.children}
 			</div>
